53ffc5f55c8853bcfeee8782d2dbb42d61f26eac
RareTop224 <butylin.098@mail.ru>
2026-02-08 23:45:57 +0300

    feat: добавил в program3 входную функцию

---
+++ program3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern void (*program2)();
+extern void (*program3)();
+
+void entry_program3(){
+	printf("Testing transition in program3\n");
+	
+	while(1){
+		printf("Working");
+	}
+}
+
+void (*program3)() = entry_program3;
