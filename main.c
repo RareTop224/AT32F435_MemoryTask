40c85b58a619f51d17526c801cf3fd37c164ecba
RareTop224 <butylin.098@mail.ru>
2026-02-10 01:39:06 +0300

    feat: добавил настройку регистров для user button

--- main.c
+++ main.c
@@ -14,11 +14,17 @@ extern int (*main_func)();
 #define RCC_AHB1ENR  (*(volatile uint32_t *)(RCC_BASE + 0x30))
 
 
-// config gpio
+// config gpio for LED
 #define GPIOD_BASE_ADDRESS (AHB1_BASE + 0x0C00UL)	
 #define GPIOD_CFGR (*(volatile uint32_t *)(GPIOD_BASE_ADDRESS + 0x00))		// gpio mode configuration
 #define GPIOD_ODT (*(volatile uint32_t *)(GPIOD_BASE_ADDRESS + 0x14))		// the output status of I/O port.
 #define GPIOD_SCR (*(volatile uint32_t *)(GPIOD_BASE_ADDRESS + 0x18))		// gpio set/clear bit
+
+// config gpio for the user button
+#define GPIOA_BASE_ADDRESS (AHB1_BASE + 0x0000UL)
+#define GPIOA_CFGR (*(volatile uint32_t *)(GPIOD_BASE_ADDRESS + 0x00))		// gpio mode configuration
+#define GPIOA_PUDR   (*(volatile uint32_t *)(GPIOA_BASE + 0x0C)) // pull-up/down
+#define GPIOA_IDR (*(volatile uint32_t *)(GPIOA_BASE + 0x10))		// input data
 	
 void blink_led(void){
 	GPIOD_SCR = (1 << 13);
@@ -30,7 +36,8 @@ void blink_led(void){
 
 int main(void){
 	printf("Main program\n");
-	RCC_AHB1ENR |= (1 << 3);	// enable clocking for pin D
+	RCC_AHB1ENR |= (1 << 3);	// enable clocking GPIOD
+	RCC_AHB1ENR |= (1 << 0);	// enable clocking GPIOA
 	
 	GPIOD_CFGR &= ~(3 << (13 * 2));  
 	GPIOD_CFGR |=  (1 << (13 * 2));	// PD13 output 
