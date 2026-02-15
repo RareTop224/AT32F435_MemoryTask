ecd7a8ff4868630f1a2c9826619853f65c7c9972
RareTop224 <butylin.098@mail.ru>
2026-02-15 23:41:39 +0300

    refactor(main): изменил формат инициализации GPIO
    - добавил структуру инициализации с полями необходимых регистров (GPIO)

--- main.c
+++ main.c
@@ -7,27 +7,33 @@
 extern void (*program2)();
 extern int (*main_func)();
 
-#define RCC_AHB1ENR (*(volatile uint32_t *)(0x40023830UL))
-	
-// config gpio for LED
-#define GPIOD_BASE_ADDRESS 0x40020C00UL	
-#define GPIOD_CFGR (*(volatile uint32_t *)(GPIOD_BASE_ADDRESS + 0x00))		// gpio mode configuration
-#define GPIOD_ODT (*(volatile uint32_t *)(GPIOD_BASE_ADDRESS + 0x14))		// the output status of I/O port.
-#define GPIOD_SCR (*(volatile uint32_t *)(GPIOD_BASE_ADDRESS + 0x18))		// gpio set/clear bit
+typedef struct {
+	volatile uint32_t CFGR;		// gpio mode configuration
+	volatile uint32_t ODT;	// the output status of I/O port.
+	volatile uint32_t IDT;	// input data
+	volatile uint32_t PULL;		// pull-up/down
+	volatile uint32_t SCR;		// gpio set/clear bit
+} GPIO_TypeDef;
 
-// config gpio for the user button
-#define GPIOA_BASE_ADDRESS 0x40020000UL	
-#define GPIOA_CFGR (*(volatile uint32_t *)(GPIOA_BASE_ADDRESS + 0x00))		// gpio mode configuration
-#define GPIOA_PUDR (*(volatile uint32_t *)(GPIOA_BASE_ADDRESS + 0x0C)) // pull-up/down
-#define GPIOA_IDR (*(volatile uint32_t *)(GPIOA_BASE_ADDRESS + 0x10))		// input data
+
+#define RCC_AHB1ENR 	(*(volatile uint32_t *)(0x40023830))
+
+#define PERIHP_BASE 	0x40000000
+
+#define GPIOD_BASE_ADDRESS 	(PERIPH_BASE + 0x00000400)	// port D
+#define GPIOA_BASE_ADDRESS 	(PERIPH_BASE + 0x00000C00) 	// port A
+
+#define LED ((GPIO_TypeDef *) GPIOD_BASE_ADDRESS)
+#define button ((GPIO_TypeDef *) GPIOD_BASE_ADDRESS)
 
 
 void blink_led(void){
-	GPIOD_SCR = (1 << 13);
+	
+	LED->SCR = (1 << 13);
 	for (volatile int i=0; i<5000; i++){}
 	//delay_ms(500);
 		
-	GPIOD_SCR = (1 << (13 + 16));
+	LED->SCR  = (1 << (13 + 16));
 	for (volatile int i=0; i<5000; i++){}
 	//delay_ms(500);
 }
@@ -37,13 +43,11 @@ int main(void){
 	RCC_AHB1ENR |= (1 << 3);	// enable clocking GPIOD
 	RCC_AHB1ENR |= (1 << 0);	// enable clocking GPIOA
 	
-	GPIOD_CFGR &= ~(3 << (13 * 2)); 
-	GPIOD_CFGR |= (1 << (13 * 2));	// PD13 output 
+	LED->CFGR &= ~(3 << (13 * 2)); 
+	LED->CFGR |= (1 << (13 * 2));	// PD13 output 
 	
 	while (1){
 		blink_led();
-		// GPIOD_SCR ^= (1 << 14);
-    // for (volatile int i=0; i<500000; i++){}
 	}
 }
 
