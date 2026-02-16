7101f33cd8d9667fb3bc9b499fa5ce7e3540c295
RareTop224 <butylin.098@mail.ru>
2026-02-16 22:33:43 +0300

    feat(main): добавил инициализацию usart

--- main.c
+++ main.c
@@ -17,17 +17,28 @@ typedef struct {
 	volatile uint32_t SCR;		// gpio set/clear bit
 } GPIO_TypeDef;
 
+typedef struct{
+	volatile uint32_t USART_STS;
+	volatile uint32_t USART_DT;
+	volatile uint32_t USART_BAUDR;
+	volatile uint32_t USART_CTRL1;	// 0x0C
+	volatile uint32_t USART_CTRL2;
+	volatile uint32_t USART_CTRL3;
+	volatile uint32_t USART_GTP;
+} USART_TypeDef;
 
 #define RCC_AHB1ENR 	(*(volatile uint32_t *)(0x40023830))
+// #define RCC_APB2ENR		(*(volatile uint32_t *)(0x40010044))
 
-#define PERIHP_BASE 	0x40000000
+#define PERIPHERY_BASE 	0x40000000
 
-#define GPIOD_BASE_ADDRESS 	(PERIPH_BASE + 0x00000400)	// port D
-#define GPIOA_BASE_ADDRESS 	(PERIPH_BASE + 0x00000C00) 	// port A
+#define GPIOD_BASE_ADDRESS 	(PERIPHERY_BASE + 0x00000400)	// port D
+#define GPIOA_BASE_ADDRESS 	(PERIPHERY_BASE + 0x00000C00) 	// port A
+#define USART_ADDRESS (PERIPHERY_BASE  + 0x00011000)
 
 #define LED ((GPIO_TypeDef *) GPIOD_BASE_ADDRESS)
 #define button ((GPIO_TypeDef *) GPIOD_BASE_ADDRESS)
-
+#define usart_print ((USART_TypeDef *) USART_ADDRESS)
 
 void blink_led(void){
 	
@@ -44,7 +55,8 @@ int main(void){
 	//printf("Main program\n");
 	RCC_AHB1ENR |= (1 << 3);	// enable clocking GPIOD
 	RCC_AHB1ENR |= (1 << 0);	// enable clocking GPIOA
-	
+	usart_print->USART_CTRL1 |= (1 << 4);		// enable USART
+
 	LED->CFGR &= ~(3 << (13 * 2)); 
 	LED->CFGR |= (1 << (13 * 2));	// PD13 output 
 	
